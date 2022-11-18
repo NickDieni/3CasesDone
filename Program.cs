ac1195d45150b50a502aedca48b148e459e64fcf
NickDieni <nickevereye@gmail.com>
2022-11-18 08:47:21 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ClassLibrary_For_3Cases;
+
+namespace _3Cases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutPut op = new OutPut();
+
+            bool debug;
+            Console.WriteLine("[D] Debug    [S] Standard");
+            string tast = Convert.ToString(Console.ReadKey().KeyChar).ToLower();
+
+            if (tast == "d")
+            {
+                debug = true;
+            }
+            else
+            {
+                debug = false;
+            }
+            Passaccess nyPass = new Passaccess();
+            nyPass.Kode(debug);
+
+            //Pickmenu nyStart = new Pickmenu();
+            //nyStart.Vælg(debug);
+        }
+    }
+}
