32c264950fe966eb48b1d18244174272fc75c848
NickDieni <nickevereye@gmail.com>
2022-11-18 12:36:16 +0100

    Lavet programmering færdig sammen med kommentar

--- Program.cs
+++ Program.cs
@@ -8,19 +8,22 @@ namespace _3Cases
         static void Main(string[] args)
         {
             OutPut op = new OutPut();
-
+            
+            //Får brugeren til at vælge debug eller standard
             bool debug;
             Console.WriteLine("[D] Debug    [S] Standard");
             string tast = Convert.ToString(Console.ReadKey().KeyChar).ToLower();
-
+            //Hvis der er blivet trykket D så bliver debug true
             if (tast == "d")
             {
                 debug = true;
             }
+            //Hvis nej så bliver debug til false
             else
             {
                 debug = false;
             }
+            //Efter det går den til Passaccess og køre programmet alt efter hvad man har valgt
             Passaccess nyPass = new Passaccess();
             nyPass.Kode(debug);
 
